dc97a5e7d453785cad567717e51d1b6cc771770f
Adeiza <dev@wealthparadigm.org>
2021-09-11 01:03:47 +0100

    initial commit for anoze

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="The retail gas industry managment software." />
+    <meta name="tags" content="Petrol, Gas, Diesel, Kerosene" />
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>Anoze.co</title>
+
+</head>
+
+<body class="antialiased">
+    <div class="relative flex items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                <div class="ml-4 text-center text-gray-500 sm:text-right sm:ml-0">
+                    Built with &hearts; in Nigeria.
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
