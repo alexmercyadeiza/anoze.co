2dfab07e4eca8822f6211945e823a538f4719dc7
Adeiza <dev@wealthparadigm.org>
2021-10-02 18:12:01 +0100

    add sales record

--- app/Http/Livewire/Manager/RecordSales.php
+++ app/Http/Livewire/Manager/RecordSales.php
@@ -3,9 +3,77 @@
 namespace App\Http\Livewire\Manager;
 
 use Livewire\Component;
+use App\Models\FuelPrices;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Manager;
+use App\Models\Sales;
+use Carbon\Carbon;
 
 class RecordSales extends Component
 {
+
+    public $petrol;
+    public $gas;
+    public $kerosene;
+    public $diesel;
+
+    // Get Company
+    public function record()
+    {
+        $this->validate([
+            'kerosene' => 'required',
+            'gas' => 'required',
+            'petrol' => 'required',
+            'diesel' => 'required'
+        ]);
+
+        // Get the company ID
+        $cid = Manager::select('cid')->where('id', Auth::id())->sum('cid');
+        $fid = Manager::select('cid')->where('id', Auth::id())->sum('fid');
+
+        // Check no entry already exists.
+
+        $date = Sales::where('fid', $fid)->select('created_at')->orderBy('created_at', 'DESC')->first();
+
+        //check if the date is the same, and notify user that record has already been created
+        if (Carbon::parse()->toDateString() == Carbon::parse($date->created_at ?? Carbon::tomorrow())->toDateString()) {
+            //notify
+            $this->emitSelf('notify-saved-already');
+
+            //else store the data
+        } else {
+            // Pass id to get fuel prices for the company
+            $prices = FuelPrices::select('kerosene', 'petrol', 'gas', 'diesel')->where('cid', $cid)->get();
+
+            // Get new price values
+            $this->petrol = $this->petrol * $prices[0]->petrol;
+            $this->kerosene = $this->kerosene * $prices[0]->kerosene;
+            $this->gas = $this->gas * $prices[0]->gas;
+            $this->diesel = $this->diesel * $prices[0]->diesel;
+
+            //Store the sales for the current day
+            Sales::create([
+                'fid' => $fid,
+                'cid' => $cid,
+                'kerosene' => $this->kerosene,
+                'gas' => $this->gas,
+                'diesel' => $this->diesel,
+                'petrol' => $this->petrol
+            ]);
+
+            $this->reset();
+
+            redirect('/payments');
+
+            //notify
+            $this->emitSelf('notify-saved');
+        }
+    }
+    // Get Fuel Prices
+
+
+    // Use Co Station ID
+
     public function render()
     {
         return view('livewire.manager.record-sales');
--- resources/views/livewire/manager/record-sales.blade.php
+++ resources/views/livewire/manager/record-sales.blade.php
@@ -20,53 +20,81 @@
         </div>
     </div>
 
+    <form wire:submit.prevent="record">
+        <div class="p-8">
+            <p class="tracking-tight text-3xl font-bold uppercase tracking-wider mb-6">Record Sales (ltrs)</p>
 
-    <div class="p-8">
+            <div class="mb-6">
 
-        <p class="tracking-tight text-3xl font-bold uppercase tracking-wider mb-6">Record Sales (ltrs)</p>
-
-        <div class="grid grid-cols-2 gap-6">
-
-            <div>
-                <label for="first-name" class="block font-bold font-medium text-gray-700">Petrol</label>
-                <input type="text"
-                    class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl border-gray-300" />
+                <div x-data=" { open: false }" x-init="
+                        @this.on('notify-saved-already', () => {
+                            if (open === false) setTimeout(() => { open = false }, 2500);
+                            open = true;
+                        })
+                    " x-show.transition.out.duration.1000ms="open" style="display: none;" class="alert alert-error">
+                    <div class="flex-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="w-6 h-6 mx-2 stroke-current">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z">
+                            </path>
+                        </svg>
+                        <label>Record has already been saved for today.</label>
+                    </div>
+                </div>
             </div>
 
-            <div>
-                <label for="first-name" class="block font-bold font-medium text-gray-700">Diesel</label>
-                <input type="text"
-                    class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl border-gray-300" />
-            </div>
+            <div class="grid grid-cols-2 gap-6">
+                <div>
+                    <label for="first-name" class="block font-bold font-medium text-gray-700">Petrol</label>
+                    <input type="text" wire:model="petrol"
+                        class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl @error('petrol') border-red-500 @enderror border-gray-300"
+                        placeholder="Type (0) if no sales." />
+                </div>
 
-            <div>
-                <label for="first-name" class="block font-bold font-medium text-gray-700">Kerosene</label>
-                <input type="text"
-                    class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl border-gray-300" />
-            </div>
+                <div>
+                    <label for="first-name" class="block font-bold font-medium text-gray-700">Diesel</label>
+                    <input type="text" wire:model="diesel"
+                        class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl @error('diesel') border-red-500 @enderror border-gray-300"
+                        placeholder="Type (0) if no sales." />
+                </div>
 
-            <div>
-                <label for="first-name" class="block font-bold font-medium text-gray-700">Gas (Kg)</label>
-                <input type="text"
-                    class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl border-gray-300" />
-            </div>
+                <div>
+                    <label for="first-name" class="block font-bold font-medium text-gray-700">Kerosene</label>
+                    <input type="text" wire:model="kerosene"
+                        class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl @error('kerosene') border-red-500 @enderror border-gray-300"
+                        placeholder="Type (0) if no sales." />
 
+                </div>
+
+                <div>
+                    <label for="first-name" class="block font-bold font-medium text-gray-700">Gas (Kg)</label>
+                    <input type="text" wire:model="gas"
+                        class="mt-1 focus:ring-gray-500 focus:border-gray-500 block w-full shadow-sm sm:text-sm lg:text-3xl @error('gas') border-red-500 @enderror border-gray-300"
+                        placeholder="Type (0) if no sales." />
+                </div>
+            </div>
         </div>
+        <!--Navigation-->
+        <div class="p-8 flex items-center space-x-3">
+            <button type="submit" class="btn btn-outline btn-lg flex items-center space-x-2">
 
-    </div>
-    <!--Navigation-->
-    <div class="p-8">
-        <button class="btn btn-outline btn-lg flex items-center space-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span> Record </span>
+            </button>
 
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-
-            <span> Record </span>
-
-        </button>
-    </div>
+            <span x-data="{ open: false }" x-init="
+                        @this.on('notify-saved', () => {
+                            if (open === false) setTimeout(() => { open = false }, 2500);
+                            open = true;
+                        })
+                    " x-show.transition.out.duration.1000ms="open" style="display: none;"
+                class="text-green-600 text-xl">Saved!</span>
+        </div>
+    </form>
 
 </div>
